f9ff0f23fed6d2aad8f73b9ebde23912e94ba119
italo32k <italo.magalhaes@gsuite.iff.edu.br>
2025-04-12 17:12:28 -0300

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+ECR_REGISTRY="843962161547.dkr.ecr.us-east-1.amazonaws.com/aws_repositorio"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
+docker build -t crud_aws .
+docker tag crud_aws:latest $ECR_REGISTRY/crud_aws:latest
+docker push $ECR_REGISTRY/crud_aws:latest
