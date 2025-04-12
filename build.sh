aa877e758144601b64cfd139638f3d8f03567727
italo32k <italo.magalhaes@gsuite.iff.edu.br>
2025-04-12 17:22:19 -0300

    Add correção no build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-ECR_REGISTRY="843962161547.dkr.ecr.us-east-1.amazonaws.com/aws_repositorio"
+ECR_REGISTRY="843962161547.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
 docker build -t crud_aws .
 docker tag crud_aws:latest $ECR_REGISTRY/crud_aws:latest
