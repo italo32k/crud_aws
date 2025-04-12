d904e57800e7f38b6ce00a0fa26ac7e8d813b5b1
italo32k <italo.magalhaes@gsuite.iff.edu.br>
2025-04-12 17:26:44 -0300

    Add correção no nome da aplicação

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ECR_REGISTRY="843962161547.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t crud_aws .
-docker tag crud_aws:latest $ECR_REGISTRY/crud_aws:latest
-docker push $ECR_REGISTRY/crud_aws:latest
+docker build -t aws_repositorio .
+docker tag aws_repositorio:latest $ECR_REGISTRY/aws_repositorio:latest
+docker push $ECR_REGISTRY/aws_repositorio:latest
