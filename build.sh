227e37028db6ef02f69ba70d653bfe4e71136302
Ítalo Magalhães de Sales <140966791+italo32k@users.noreply.github.com>
2025-05-08 16:07:41 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-ECR_REGISTRY="843962161547.dkr.ecr.us-east-1.amazonaws.com"
+ECR_REGISTRY="507944432706.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t aws_repositorio .
-docker tag aws_repositorio:latest $ECR_REGISTRY/aws_repositorio:latest
-docker push $ECR_REGISTRY/aws_repositorio:latest
+docker build -t aws_ecr .
+docker tag aws_ecr:latest $ECR_REGISTRY/aws_ecr:latest
+docker push $ECR_REGISTRY/aws_ecr:latest
